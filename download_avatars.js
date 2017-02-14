8a817608da9e89f6e457dd74ec9a38e0f80da817
Ken <tkg214@gmail.com>
2017-02-14 03:08:42 +0000

    Add getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,15 @@
 var request = require('request');
+var GITHUB_USER = 'tkg214';
+var GITHUB_TOKEN = '8f2c2f87f0e134e4ac789cc9c3274c0375f3deb6';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
