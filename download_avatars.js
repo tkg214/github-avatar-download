ba013b70e3ee8023d9248a6e3c85a43d237392d0
Ken <tkg214@gmail.com>
2017-02-14 03:58:32 +0000

    Add body fetching

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,24 @@
 var request = require('request');
 var GITHUB_USER = 'tkg214';
 var GITHUB_TOKEN = '8f2c2f87f0e134e4ac789cc9c3274c0375f3deb6';
+var USER_AGENT = 'GitHub Avatar Downloader';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+    'User-Agent': USER_AGENT
+    }
+  }
+  request.get(options, cb);
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function callback(err, res, body) {
+    if (err) throw err;
+    console.log('Response Status Code: ', res.statusCode);
+    console.log(body);
+}
+
+getRepoContributors("jquery", "jquery", callback);
