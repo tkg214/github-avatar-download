5241941e33576f5d31469477f69603a3858f4064
Ken <tkg214@gmail.com>
2017-02-14 05:09:51 +0000

    Add updates to downloadImageURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs')
 const GITHUB_USER = 'tkg214';
 const GITHUB_TOKEN = '8f2c2f87f0e134e4ac789cc9c3274c0375f3deb6';
 const USER_AGENT = 'GitHub Avatar Downloader';
@@ -18,12 +19,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 function callback(err, res, body) {
     if (err) throw err;
     console.log('Response Status Code: ', res.statusCode);
-    const avatarList = [];
     const data = JSON.parse(body);
     for (var user of data) {
-      avatarList.push(user['avatar_url']);
+      const newPath = 'downloads/' + user.id + '.jpg';
+      const avatarURL = user.avatar_url;
+      downloadImageByURL(avatarURL, newPath);
     }
-    console.log(avatarList);
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code: ', response.statusCode, '\nResponse Content Type: ', response.headers['content-type'], '\nDownload complete.');
+    })
+    .pipe(fs.createWriteStream(filePath), console.log('Downloading', url, 'to', filePath));
 }
 
 getRepoContributors("jquery", "jquery", callback);
